24bf735e0134277a144b9b8c8c519bb24a45e677
Chuck Lauer Vose <vosechu@gmail.com>
2013-02-17 12:57:52 -0800

    embracing jekyll and liquid, polishing up the left nav

--- javascripts/main.js
+++ javascripts/main.js
@@ -13,22 +13,23 @@ var sectionHeight = function() {
 $(window).resize(sectionHeight);
 
 $(document).ready(function(){
-  $("section h1, section h2").each(function(){
-    $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
-    $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
-    $("nav ul li:first-child a").parent().addClass("active");
-  });
-  
-  $("nav ul li").on("click", "a", function(event) {
-    var position = $($(this).attr("href")).offset().top - 190;
-    $("html, body").animate({scrollTop: position}, 400);
-    $("nav ul li a").parent().removeClass("active");
-    $(this).parent().addClass("active");
-    event.preventDefault();    
-  });
-  
+  // This appears to add an anchor in the left nav for each H1/H2 in the page
+  // $("section h1, section h2").each(function(){
+  //   $("nav ul").append("<li class='tag-" + this.nodeName.toLowerCase() + "'><a href='#" + $(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,'') + "'>" + $(this).text() + "</a></li>");
+  //   $(this).attr("id",$(this).text().toLowerCase().replace(/ /g, '-').replace(/[^\w-]+/g,''));
+  //   $("nav ul li:first-child a").parent().addClass("active");
+  // });
+  // This appears to add a click handler to the left nave to scroll to the h1 in the page
+  // $("nav ul li").on("click", "a", function(event) {
+  //   var position = $($(this).attr("href")).offset().top - 190;
+  //   $("html, body").animate({scrollTop: position}, 400);
+  //   $("nav ul li a").parent().removeClass("active");
+  //   $(this).parent().addClass("active");
+  //   event.preventDefault();
+  // });
+
   sectionHeight();
-  
+
   $('img').load(sectionHeight);
 });
 
